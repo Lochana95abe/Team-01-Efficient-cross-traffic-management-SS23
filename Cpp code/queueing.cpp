271d0452be6885fc8f82681532dcd9d705db6b9f
Heansuh <49907531+heansuh@users.noreply.github.com>
2023-04-24 07:20:38 +0200

    Update queueing.cpp

--- Cpp code/queueing.cpp
+++ Cpp code/queueing.cpp
@@ -3,7 +3,6 @@
 // Car Class
 Car::Car(int id, int speed, int lane, int dest, int enter_time) {
     this->id = id;
-    this->speed = speed;
     this->lane = lane;
     this->dest = dest;
     this->enter_time = enter_time;
@@ -36,41 +35,6 @@ int Car::get_pos() {
 
 void Car::move() {
     this->pos += this->speed;
-}
-
-// Street Class
-Street::Street(int length, int num_lanes) {
-    this->length = length;
-    this->num_lanes = num_lanes;
-    this->lanes = new Lane*[num_lanes];
-    for (int i = 0; i < num_lanes; i++) {
-        this->lanes[i] = new Lane(length);
-    }
-}
-
-Street::~Street() {
-    for (int i = 0; i < num_lanes; i++) {
-        delete lanes[i];
-    }
-    delete[] lanes;
-}
-
-int Street::get_length() {
-    return this->length;
-}
-
-int Street::get_num_lanes() {
-    return this->num_lanes;
-}
-
-Lane* Street::get_lane(int i) {
-    return this->lanes[i];
-}
-
-void Street::move() {
-    for (int i = 0; i < num_lanes; i++) {
-        lanes[i]->move();
-    }
 }
 
 // Lane Class
@@ -145,76 +109,30 @@ bool Intersection::is_junction_clear() {
 }
 
 // Car1's function
-void carOne(queue<Car>& q1, std::mutex& mtx, std::condition_variable& cv) {
+void carOne() {
     while (true) {
         // If there's no car in queue, wait
-        std::unique_lock<std::mutex> lck(mtx);
-        cv.wait(lck, [&q1] { return !q1.empty(); });
 
         // Get the first car in queue
-        Car car = q1.front();
-        q1.pop();
-        lck.unlock();
 
         // Check if the junction is clear or not
-        bool clear = false;
-        while (!clear) {
-            std::lock_guard<std::mutex> lck(mtx);
-            clear = priority_check(car, q1);
-        }
 
         // Enter junction and make the desired move
-        enter_junction(car);
-        move(car, rLane);
-        exit_junction(car);
-        move(car, rLane);
-        exit_street(car);
-
-        // Notify that the car has finished
-        {
-            std::lock_guard<std::mutex> lck(mtx);
-            finished_cars++;
-            if (finished_cars == TOTAL_CARS) {
-                cv.notify_all();
-                break;
-            }
-        }
     }
 }
 
 // Car2's function
-void carTwo(queue<Car>& q2, std::mutex& mtx, std::condition_variable& cv) {
+void carTwo() {
     while (true) {
         // If there's no car in queue, wait
-        std::unique_lock<std::mutex> lck(mtx);
-        cv.wait(lck, [&q2] { return !q2.empty(); });
 
         // Get the first car in queue
-        Car car = q2.front();
-        q2.pop();
-        lck.unlock();
 
         // Wait until the junction is clear
-        {
-            std::lock_guard<std::mutex> lck(mtx);
-            while (!priority_check(car, q1)) {}
-        }
 
         // Enter junction and make the desired move
-        enter_junction(car);
-        move(car, lLane);
-        exit_junction(car);
-        move(car, lLane);
-        exit_street(car);
 
         // Notify that the car has finished
-        {
-            std::lock_guard<std::mutex> lck(mtx);
-            finished_cars++;
-            if (finished_cars == TOTAL_CARS) {
-                cv.notify_all();
-                break;
-            }
-        }
+
     }
 }
